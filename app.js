750db17e2c4bdb30b438c1d27753d8d6d42b7774
Okpe onoja <okpeonoja18@gmail.com>
2022-11-21 00:12:23 +0100

    first push before refactoring

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get("/contact", function(req, res){
 
 app.get("/compose", function(req, res){
   res.render("compose");
-});
+}); 
 
 app.post("/compose", function(req, res){
   const post = {
